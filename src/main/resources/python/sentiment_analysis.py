13bd1273bce1eb41575faaddbe33106919d600ee
larzarus0320 <astar5327z@gmail.com>
2024-06-15 12:13:45 +0900

    Fix: 예외처리 및 부정 단어 추가

--- src/main/resources/python/sentiment_analysis.py
+++ src/main/resources/python/sentiment_analysis.py
@@ -4,12 +4,26 @@ from nltk.tokenize import sent_tokenize
 from nltk.sentiment import SentimentIntensityAnalyzer
 
 def sentiment_analysis(answer):
+
+    if answer[0].islower():
+        return -2.0
+
+    negative_phrases = [
+        'I’m sorry', 'I am sorry', 'sorry', 'Sorry', 'I cannot', 'I can’t',
+        'It’s important', 'It is important', 'As an AI', 'As a language model', 'This statement'
+    ]
+
+    for phrase in negative_phrases:
+        if phrase in answer:
+            return -1.0
+
     sentences = sent_tokenize(answer)
     compound_scores = []
     sid = SentimentIntensityAnalyzer()
     for sentence in sentences:
         scores = sid.polarity_scores(sentence)
         compound_scores.append(scores['compound'])
+        break
     average_compound_score = sum(compound_scores) / len(compound_scores)
     return average_compound_score
 
