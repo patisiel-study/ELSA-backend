5ed36e1bde054288fcee2dedc0371736e60ce40e
lazarus0320 <astar5327z@gmail.com>
2024-11-23 20:01:03 +0900

    Fix: https, http 설정

--- src/main/java/com/example/elsa/global/config/WebConfig.java
+++ src/main/java/com/example/elsa/global/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://humaind.co.kr:3000") // 리액트 애플리케이션의 도메인 및 포트 번호
+			.allowedOrigins("https://humaind.co.kr", "http://localhost:3000") // 리액트 애플리케이션의 도메인 및 포트 번호
 			.allowedMethods("*") // OPTIONS 메서드 추가
 			.allowedHeaders("*")
 			.allowCredentials(true)
